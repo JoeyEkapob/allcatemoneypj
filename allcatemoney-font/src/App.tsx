875fc02f1e3515fed8b20c6ebc2cb97303008e7a
JoeyEkapob <honhon16461@gmail.com>
2025-03-27 22:50:39 +0700

    Initial commit

---
+++ allcatemoney-font/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind CSS!</h1>
+      <p className="mt-4 text-gray-700">ยินดีต้อนรับสู่ React + Tailwind CSS</p>
+    </div>
+  );
+}
+
+export default App;
