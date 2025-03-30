e7ffd4a606422c4c31f6f0b52e11e83eeb68481f
JoeyEkapob <honhon16461@gmail.com>
2025-03-30 11:14:55 +0700

    setup tailwincss font setup not success

--- allcatemoney-font/src/App.tsx
+++ allcatemoney-font/src/App.tsx
@@ -1,5 +1,7 @@
 import LoginPage from "./pages/login/LoginPage.jsx"
 
+
+
 export default function App(){
   return (
     <div>
--- allcatemoney-font/src/pages/login/LoginPage.jsx
+++ allcatemoney-font/src/pages/login/LoginPage.jsx
@@ -3,7 +3,7 @@ import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
 
 const InputField = ({ label, type, value, onChange, error }) => (
   <div>
-    <label className="block text-gray-700">{label}</label>
+    <label className="font-sans  block text-gray-700">{label}</label>
     <input
       type={type}
       value={value}
@@ -56,12 +56,14 @@ const LoginPage = () => {
 
 
   return (
+   
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
+      
       <div className="w-full max-w-md p-6 bg-white rounded-2xl shadow-lg">
-        <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
+        <h2 className="font-sans text-2xl font-bold text-center text-gray-800 mb-6 ">
           เข้าสู่ระบบ
         </h2>
-
+    
         <form onSubmit={handleSubmit} className="space-y-4">
           <InputField
             label="อีเมล"
